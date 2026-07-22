47c7ab850d8dceea692869a805e193eec99d70e9
Faros <114036707+a-faros@users.noreply.github.com>
2026-07-22 15:33:28 +0800

    Automated bulk repository update

---
+++ src/OrganizationServiceProvider.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Rimba\Organization;
+
+use Rimba\Base\BitesServiceProvider;
+
+
+class OrganizationServiceProvider extends BitesServiceProvider
+{
+
+    protected function bootPackage(): void
+    {
+        //
+    }
+    protected function registerPackage(): void
+    {
+        //
+    }
+
+}
