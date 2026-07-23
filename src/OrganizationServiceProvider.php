d4184d8137f75d5ff1b7415e12267c4f6c87d745
Faros <114036707+a-faros@users.noreply.github.com>
2026-07-23 15:46:24 +0800

    Automated Update 2026-07-23 15:46

--- src/OrganizationServiceProvider.php
+++ src/OrganizationServiceProvider.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace Rimba\Organization;
 
-use Rimba\Base\BitesServiceProvider;
+use Rimba\Base\Services\BitesServiceProvider;
 
 class OrganizationServiceProvider extends BitesServiceProvider
 {
