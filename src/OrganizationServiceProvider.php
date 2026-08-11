7d6795dabd67737257f2ac0b10b5164034324d3d
Faros <114036707+a-faros@users.noreply.github.com>
2026-08-11 16:16:57 +0800

    Release v1.2.10

--- src/OrganizationServiceProvider.php
+++ src/OrganizationServiceProvider.php
@@ -6,16 +6,18 @@ namespace Rimba\Organization;
 
 use Rimba\Base\Services\BitesServiceProvider;
 
+
 class OrganizationServiceProvider extends BitesServiceProvider
 {
+
     protected function bootPackage(): void
     {
         $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
         //
     }
-
     protected function registerPackage(): void
     {
         //
     }
+
 }
